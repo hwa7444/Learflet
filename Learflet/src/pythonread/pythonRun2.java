88d7c77f914c91374570b5e8191cba11dd214f89
Byungwoo <kbw0519@naver.com>
2018-01-07 16:22:26 +0900

    pythonrun2

---
+++ Learflet/src/pythonread/pythonRun2.java
@@ -0,0 +1,60 @@
+package pythonread;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class pythonRun2 {
+   public static void main(String[] args) throws IOException, InterruptedException {
+      
+	   
+	   
+      String id = "byungwoo";
+      
+      StringBuffer cmd = new StringBuffer();
+      StringBuffer pout = new StringBuffer();
+      
+      cmd.append("C:\\Users\\Byungwoo\\AppData\\Local\\Programs\\Python\\Python35\\python.exe D:\\zz.py");
+      cmd.append(" ");
+      cmd.append(id);
+      
+      System.out.println(cmd.toString());
+      
+      try {
+         
+         System.out.println("java start");
+         Runtime r = Runtime.getRuntime();
+         Process p = r.exec(cmd.toString());
+         
+         int resultSign = -1;
+            
+         try{
+            resultSign = p.waitFor();
+         }catch(Exception e) {
+            System.out.println(e);
+         }
+         if(resultSign == 0) {
+            
+            System.out.println("Python operation success");
+            BufferedReader bfr = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            
+            String line = "";
+            while((line = bfr.readLine()) != null) {
+               pout.append(line);
+               pout.append("\n");
+            }
+            
+            bfr.close();
+            
+            System.out.println(pout.toString());
+         } else {
+            System.out.println(resultSign);
+         }
+         
+         System.out.println("java end");
+      } catch (Exception e) {
+         System.out.println(e);
+      }
+   }
+
+}
