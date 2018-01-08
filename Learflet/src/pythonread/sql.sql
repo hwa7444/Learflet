ab9c04f1eddc7f7e7d5863d28796e5ff579a7fc0
Byungwoo <kbw0519@naver.com>
2018-01-08 17:32:52 +0900

    다시

---
+++ Learflet/src/pythonread/sql.sql
@@ -0,0 +1,98 @@
+--------------------------------------------------------
+--  파일이 생성됨 - 월요일-1월-08-2018   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Table ANALDATA
+--------------------------------------------------------
+
+  CREATE TABLE "ANALDATA" 
+   (	"ID" VARCHAR2(20), 
+	"AGE" VARCHAR2(20), 
+	"GENDER" VARCHAR2(20), 
+	"INTEREST1" VARCHAR2(20), 
+	"INTEREST2" VARCHAR2(20), 
+	"INTEREST3" VARCHAR2(20), 
+	"COUPON1" VARCHAR2(20), 
+	"COUPON2" VARCHAR2(20), 
+	"COUPON3" VARCHAR2(20)
+   )
+REM INSERTING into ANALDATA
+SET DEFINE OFF;
+Insert into ANALDATA (ID,AGE,GENDER,INTEREST1,INTEREST2,INTEREST3,COUPON1,COUPON2,COUPON3) values ('asdf','ten','W','1','4','12','a','a','a');
+Insert into ANALDATA (ID,AGE,GENDER,INTEREST1,INTEREST2,INTEREST3,COUPON1,COUPON2,COUPON3) values ('asdf','earlyTwnty','W','13','7','13','b','d','None');
+Insert into ANALDATA (ID,AGE,GENDER,INTEREST1,INTEREST2,INTEREST3,COUPON1,COUPON2,COUPON3) values ('we','lateTwnty','W','5','13','7','c','e','None');
+Insert into ANALDATA (ID,AGE,GENDER,INTEREST1,INTEREST2,INTEREST3,COUPON1,COUPON2,COUPON3) values ('f','earlyThty','W','9','4','7','d','None','None');
+Insert into ANALDATA (ID,AGE,GENDER,INTEREST1,INTEREST2,INTEREST3,COUPON1,COUPON2,COUPON3) values ('asdf','lateThty','W','2','2','4','b','f','None');
+Insert into ANALDATA (ID,AGE,GENDER,INTEREST1,INTEREST2,INTEREST3,COUPON1,COUPON2,COUPON3) values ('fz','fourty','W','4','15','8','e','None','None');
+Insert into ANALDATA (ID,AGE,GENDER,INTEREST1,INTEREST2,INTEREST3,COUPON1,COUPON2,COUPON3) values ('cv','fifty','W','15','14','9','f','z','f');
+Insert into ANALDATA (ID,AGE,GENDER,INTEREST1,INTEREST2,INTEREST3,COUPON1,COUPON2,COUPON3) values ('asdf','sixty','W','10','13','13','g','None','None');
+Insert into ANALDATA (ID,AGE,GENDER,INTEREST1,INTEREST2,INTEREST3,COUPON1,COUPON2,COUPON3) values ('df','ten','W','8','10','13','a','x','None');
+Insert into ANALDATA (ID,AGE,GENDER,INTEREST1,INTEREST2,INTEREST3,COUPON1,COUPON2,COUPON3) values ('awe','earlyTwnty','W','6','11','2','a','a','None');
+Insert into ANALDATA (ID,AGE,GENDER,INTEREST1,INTEREST2,INTEREST3,COUPON1,COUPON2,COUPON3) values ('ga','lateTwnty','W','11','6','12','q','v','None');
+Insert into ANALDATA (ID,AGE,GENDER,INTEREST1,INTEREST2,INTEREST3,COUPON1,COUPON2,COUPON3) values ('sdf','earlyThty','W','13','1','5','d','a','s');
+Insert into ANALDATA (ID,AGE,GENDER,INTEREST1,INTEREST2,INTEREST3,COUPON1,COUPON2,COUPON3) values ('z','lateThty','W','12','5','9','q','a','None');
+Insert into ANALDATA (ID,AGE,GENDER,INTEREST1,INTEREST2,INTEREST3,COUPON1,COUPON2,COUPON3) values ('xcv','fourty','W','13','1','8','e','d','None');
+Insert into ANALDATA (ID,AGE,GENDER,INTEREST1,INTEREST2,INTEREST3,COUPON1,COUPON2,COUPON3) values ('ag','fifty','W','9','13','5','r','r','None');
+Insert into ANALDATA (ID,AGE,GENDER,INTEREST1,INTEREST2,INTEREST3,COUPON1,COUPON2,COUPON3) values ('awe','sixty','W','11','15','3','t','f','d');
+Insert into ANALDATA (ID,AGE,GENDER,INTEREST1,INTEREST2,INTEREST3,COUPON1,COUPON2,COUPON3) values ('ef','ten','W','15','15','7','y','a','d');
+Insert into ANALDATA (ID,AGE,GENDER,INTEREST1,INTEREST2,INTEREST3,COUPON1,COUPON2,COUPON3) values ('asdf','earlyTwnty','W','11','6','6','u','d','g');
+Insert into ANALDATA (ID,AGE,GENDER,INTEREST1,INTEREST2,INTEREST3,COUPON1,COUPON2,COUPON3) values ('fas','lateTwnty','W','11','7','8','i','g','None');
+Insert into ANALDATA (ID,AGE,GENDER,INTEREST1,INTEREST2,INTEREST3,COUPON1,COUPON2,COUPON3) values ('df','earlyThty','W','1','4','15','h','None','None');
+Insert into ANALDATA (ID,AGE,GENDER,INTEREST1,INTEREST2,INTEREST3,COUPON1,COUPON2,COUPON3) values ('asdf','lateThty','M','2','8','2','d','h','None');
+Insert into ANALDATA (ID,AGE,GENDER,INTEREST1,INTEREST2,INTEREST3,COUPON1,COUPON2,COUPON3) values ('wa','fourty','M','12','14','7','d','a','g');
+Insert into ANALDATA (ID,AGE,GENDER,INTEREST1,INTEREST2,INTEREST3,COUPON1,COUPON2,COUPON3) values ('ef','fifty','M','11','14','13','g','t','None');
+Insert into ANALDATA (ID,AGE,GENDER,INTEREST1,INTEREST2,INTEREST3,COUPON1,COUPON2,COUPON3) values ('z','sixty','M','7','5','13','h','None','None');
+Insert into ANALDATA (ID,AGE,GENDER,INTEREST1,INTEREST2,INTEREST3,COUPON1,COUPON2,COUPON3) values ('cvz','ten','M','2','2','4','j','x','None');
+Insert into ANALDATA (ID,AGE,GENDER,INTEREST1,INTEREST2,INTEREST3,COUPON1,COUPON2,COUPON3) values ('xcv','earlyTwnty','M','9','1','6','s','r','b');
+Insert into ANALDATA (ID,AGE,GENDER,INTEREST1,INTEREST2,INTEREST3,COUPON1,COUPON2,COUPON3) values ('asdf','lateTwnty','M','13','2','4','v','None','None');
+Insert into ANALDATA (ID,AGE,GENDER,INTEREST1,INTEREST2,INTEREST3,COUPON1,COUPON2,COUPON3) values ('w','earlyThty','M','15','6','15','b','a','None');
+Insert into ANALDATA (ID,AGE,GENDER,INTEREST1,INTEREST2,INTEREST3,COUPON1,COUPON2,COUPON3) values ('asdf','lateThty','M','6','7','12','n','None','None');
+Insert into ANALDATA (ID,AGE,GENDER,INTEREST1,INTEREST2,INTEREST3,COUPON1,COUPON2,COUPON3) values ('sdf','fourty','M','2','3','2','k','g','b');
+Insert into ANALDATA (ID,AGE,GENDER,INTEREST1,INTEREST2,INTEREST3,COUPON1,COUPON2,COUPON3) values ('dva','fifty','M','7','7','5','j','None','None');
+Insert into ANALDATA (ID,AGE,GENDER,INTEREST1,INTEREST2,INTEREST3,COUPON1,COUPON2,COUPON3) values ('sdf','sixty','M','7','1','2','j','h','b');
+Insert into ANALDATA (ID,AGE,GENDER,INTEREST1,INTEREST2,INTEREST3,COUPON1,COUPON2,COUPON3) values ('a','ten','M','5','11','5','j','d','None');
+Insert into ANALDATA (ID,AGE,GENDER,INTEREST1,INTEREST2,INTEREST3,COUPON1,COUPON2,COUPON3) values ('ef','earlyTwnty','M','15','7','13','k','t','None');
+Insert into ANALDATA (ID,AGE,GENDER,INTEREST1,INTEREST2,INTEREST3,COUPON1,COUPON2,COUPON3) values ('w','lateTwnty','M','10','6','9','l','o','None');
+Insert into ANALDATA (ID,AGE,GENDER,INTEREST1,INTEREST2,INTEREST3,COUPON1,COUPON2,COUPON3) values ('ef','earlyThty','M','7','10','12','p','f','None');
+Insert into ANALDATA (ID,AGE,GENDER,INTEREST1,INTEREST2,INTEREST3,COUPON1,COUPON2,COUPON3) values ('f','lateThty','M','10','9','4','i','o','None');
+Insert into ANALDATA (ID,AGE,GENDER,INTEREST1,INTEREST2,INTEREST3,COUPON1,COUPON2,COUPON3) values ('a','fourty','M','7','14','13','a','s','l');
+Insert into ANALDATA (ID,AGE,GENDER,INTEREST1,INTEREST2,INTEREST3,COUPON1,COUPON2,COUPON3) values ('sdf','fifty','M','12','2','1','z','o','None');
+Insert into ANALDATA (ID,AGE,GENDER,INTEREST1,INTEREST2,INTEREST3,COUPON1,COUPON2,COUPON3) values ('f','sixty','M','1','6','10','x','f','None');
+Insert into ANALDATA (ID,AGE,GENDER,INTEREST1,INTEREST2,INTEREST3,COUPON1,COUPON2,COUPON3) values ('byungwoo','a','a','1','2','3','e','hi','hi');
+Insert into ANALDATA (ID,AGE,GENDER,INTEREST1,INTEREST2,INTEREST3,COUPON1,COUPON2,COUPON3) values ('zzz','zzz','zzz','zzz','zzz','zzz','None','None','None');
+
+
+
+--------------------------------------------------------
+--  파일이 생성됨 - 월요일-1월-08-2018   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Table COUPONRANK
+--------------------------------------------------------
+
+  CREATE TABLE "COUPONRANK" 
+   (	"COUPON1" VARCHAR2(20), 
+	"COUPON2" VARCHAR2(20), 
+	"COUPON3" VARCHAR2(20), 
+	"COUPON4" VARCHAR2(20), 
+	"COUPON5" VARCHAR2(20), 
+	"COUPON6" VARCHAR2(20)
+   )
+REM INSERTING into COUPONRANK
+SET DEFINE OFF;
+Insert into COUPONRANK (COUPON1,COUPON2,COUPON3,COUPON4,COUPON5,COUPON6) values ('None','s','g','a','d','b');
+Insert into COUPONRANK (COUPON1,COUPON2,COUPON3,COUPON4,COUPON5,COUPON6) values ('a','None','f','s','d','g');
+Insert into COUPONRANK (COUPON1,COUPON2,COUPON3,COUPON4,COUPON5,COUPON6) values ('None','s','g','a','d','b');
+Insert into COUPONRANK (COUPON1,COUPON2,COUPON3,COUPON4,COUPON5,COUPON6) values ('a','None','f','s','d','g');
+Insert into COUPONRANK (COUPON1,COUPON2,COUPON3,COUPON4,COUPON5,COUPON6) values ('a','None','f','s','d','g');
+Insert into COUPONRANK (COUPON1,COUPON2,COUPON3,COUPON4,COUPON5,COUPON6) values ('a','None','f','s','d','g');
+Insert into COUPONRANK (COUPON1,COUPON2,COUPON3,COUPON4,COUPON5,COUPON6) values ('None','s','g','a','d','b');
+Insert into COUPONRANK (COUPON1,COUPON2,COUPON3,COUPON4,COUPON5,COUPON6) values ('None','s','g','a','d','b');
+Insert into COUPONRANK (COUPON1,COUPON2,COUPON3,COUPON4,COUPON5,COUPON6) values ('None','s','g','a','d','b');
+Insert into COUPONRANK (COUPON1,COUPON2,COUPON3,COUPON4,COUPON5,COUPON6) values ('None','s','g','a','d','b');
+Insert into COUPONRANK (COUPON1,COUPON2,COUPON3,COUPON4,COUPON5,COUPON6) values ('None','s','g','a','d','b');
+Insert into COUPONRANK (COUPON1,COUPON2,COUPON3,COUPON4,COUPON5,COUPON6) values ('None','s','g','a','d','b');
+Insert into COUPONRANK (COUPON1,COUPON2,COUPON3,COUPON4,COUPON5,COUPON6) values ('None','s','g','a','d','b');
+Insert into COUPONRANK (COUPON1,COUPON2,COUPON3,COUPON4,COUPON5,COUPON6) values ('None','s','g','a','d','b');
+Insert into COUPONRANK (COUPON1,COUPON2,COUPON3,COUPON4,COUPON5,COUPON6) values ('None','s','g','a','d','b');
+Insert into COUPONRANK (COUPON1,COUPON2,COUPON3,COUPON4,COUPON5,COUPON6) values ('None','s','g','a','d','b');
