d56394bc2e09d3a2c898ebd42845b70943555b21
pc-07 <pc-07@www.testdomain.com>
2018-01-09 17:48:28 +0900

    서버성공

--- Learflet/src/com/service/mainCon.java
+++ Learflet/src/com/service/mainCon.java
@@ -2,6 +2,7 @@ package com.service;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,27 +12,59 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.dao.AnalDAO;
 import com.dao.AnalVO;
+import com.dao.couponDAO;
+import com.dao.couponrankVO;
+
+import pythonread.pythonRun2;
 
 /**
  * Servlet implementation class mainCon
  */
 @WebServlet("/mainCon")
 public class mainCon extends HttpServlet {
-	
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		
+		PrintWriter out = response.getWriter();
 		request.setCharacterEncoding("utf-8");
 		response.setCharacterEncoding("text/html; charset=utf-8");
+
+		String log = request.getParameter("join");
+		System.out.println("로그값"+log);
+		String login = request.getParameter("login");
+		System.out.println("로그인아이디"+login);
 		
-		String log = request.getParameter("log");
-		if (log!=null) {
-			if (log.equals("login")) {//로그인 시작
-				String id = request.getParameter("id");
+		
+		
+		if(login !=null) {
+			String id = request.getParameter("id");
+			System.out.println("분석페이지");
+			//request.getParameter("log");
+			couponDAO dao = couponDAO.getInstance();
+			ArrayList<couponrankVO> list;
+			pythonRun2 py = new pythonRun2();
+			py.run();
+			
+			try {
+				list = dao.selectAll();
 				
-				//로그인 끝
-			}else if(log.equals("join")) {//회원가입 시작
-				
-				String id = request.getParameter("id");
+				System.out.println(list.get(0).getCoupon1());
+				System.out.println(list.get(0).getCoupon2());
+				out.println(list.get(0).getCoupon1() + "," + list.get(0).getCoupon2() + ","
+						+ list.get(0).getCoupon3() + "," + list.get(0).getCoupon4());
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			// 로그인 끝
+		}
+		
+		
+		
+		if (log != null) {
+			if (log.equals("join")) {// 회원가입 시작
+				String id = request.getParameter("joinid");
 				String age = request.getParameter("age");
 				String gender = request.getParameter("gender");
 				String interest1 = request.getParameter("interest1");
@@ -39,31 +72,33 @@ public class mainCon extends HttpServlet {
 				String interest3 = request.getParameter("interest3");
 				AnalVO vo = new AnalVO(id, age, gender, interest1, interest2, interest3);
 				AnalDAO dao = AnalDAO.getInstance();
-				
+
 				try {
-					   if (id!=null) {
+					if (id != null) {
 
-						   int cnt = dao.insertData(id, age, gender, interest1, interest2, interest3);
-						   System.out.println(age);
-						   if(cnt>0) {
-							   System.out.println("데이터 삽입 성공");
-							   
-						   }else {
-							   System.out.println("데이터 삽입 실패");
-						   }
+						int cnt = dao.insertData(id, age, gender, interest1, interest2, interest3);
+						System.out.println(age);
+						if (cnt > 0) {
+							System.out.println("데이터 삽입 성공");
+
+						} else {
+							System.out.println("데이터 삽입 실패");
+						}
 					}
-					   
-					   
-				   }catch(Exception e) {
-					   e.printStackTrace();
-				   }
 
-				 
-			}//회원가입 끝
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+
+			} // 회원가입 끝
+			else if (log.equals("analysis")) {// 분석시작
+				
+
+			} // 분석 끝
 			
+
 		}
-		
-		
+
 	}
 
 }
--- Learflet/src/pythonread/pythonRun2.java
+++ Learflet/src/pythonread/pythonRun2.java
@@ -9,52 +9,59 @@ public class pythonRun2 {
       
 	   
 	   
-      String id = "byungwoo";
       
-      StringBuffer cmd = new StringBuffer();
-      StringBuffer pout = new StringBuffer();
       
-      cmd.append("python C:\\zz.py");
-      cmd.append(" ");
-      cmd.append(id);
       
-      System.out.println(cmd.toString());
-      
-      try {
-         
-         System.out.println("java start");
-         Runtime r = Runtime.getRuntime();
-         Process p = r.exec(cmd.toString());
-         
-         int resultSign = -1;
-            
-         try{
-            resultSign = p.waitFor();
-         }catch(Exception e) {
-            System.out.println(e);
-         }
-         if(resultSign == 0) {
-            
-            System.out.println("Python operation success");
-            BufferedReader bfr = new BufferedReader(new InputStreamReader(p.getInputStream()));
-            
-            String line = "";
-            while((line = bfr.readLine()) != null) {
-               pout.append(line);
-               pout.append("\n");
-            }
-            
-            bfr.close();
-            
-            System.out.println(pout.toString());
-         } else {
-            System.out.println(resultSign);
-         }
-         
-         System.out.println("java end");
-      } catch (Exception e) {
-         System.out.println(e);
-      }
    }
+
+public void run() {
+	String id = "byungwoo";
+    
+    StringBuffer cmd = new StringBuffer();
+    StringBuffer pout = new StringBuffer();
+    
+    cmd.append("python C:\\zz.py");
+    cmd.append(" ");
+    cmd.append(id);
+    
+    System.out.println(cmd.toString());
+    
+    try {
+       
+       System.out.println("java start");
+       Runtime r = Runtime.getRuntime();
+       Process p = r.exec(cmd.toString());
+       
+       int resultSign = -1;
+          
+       try{
+          resultSign = p.waitFor();
+       }catch(Exception e) {
+          System.out.println(e);
+       }
+       if(resultSign == 0) {
+          
+          System.out.println("Python operation success");
+          BufferedReader bfr = new BufferedReader(new InputStreamReader(p.getInputStream()));
+          
+          String line = "";
+          while((line = bfr.readLine()) != null) {
+             pout.append(line);
+             pout.append("\n");
+          }
+          
+          bfr.close();
+          
+          System.out.println(pout.toString());
+       } else {
+          System.out.println(resultSign);
+       }
+       
+       System.out.println("java end");
+    } catch (Exception e) {
+       System.out.println(e);
+    }
+	
+}
 
 }
